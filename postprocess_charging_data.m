969d917635ba6e3a5b72f333a1aed345855888a9
Mark Gottscho <mgottscho@ucla.edu>
2014-06-10 11:10:34 -0700

    Added pre and post processing support for more Nexus4 and rawbatt data.

--- postprocess_charging_data.m
+++ postprocess_charging_data.m
@@ -7,11 +7,12 @@
 %% Set up paths for pre-processed, processed, and output
 master_path = '/Users/Mark/Dropbox/AvailabilityAware-share/data/charging/';
 
-pre_processed_data_path = [master_path 'pre-processed/'];
-nexus4_pre_processed_data_path = [pre_processed_data_path 'nexus4/'];
-ipod_pre_processed_data_path = [pre_processed_data_path 'ipod/'];
-custom_pre_processed_data_path = [pre_processed_data_path 'custom/'];
-rawbatt_pre_processed_data_path = [pre_processed_data_path 'rawbatt/'];
+% These paths not needed right now
+% pre_processed_data_path = [master_path 'pre-processed/'];
+% nexus4_pre_processed_data_path = [pre_processed_data_path 'nexus4/'];
+% ipod_pre_processed_data_path = [pre_processed_data_path 'ipod/'];
+% custom_pre_processed_data_path = [pre_processed_data_path 'custom/'];
+% rawbatt_pre_processed_data_path = [pre_processed_data_path 'rawbatt/'];
 
 post_processed_data_path = [master_path 'post-processed/'];
 nexus4_post_processed_data_path = [post_processed_data_path 'nexus4/'];
@@ -32,7 +33,7 @@ nexus4_trace_5V_400mA_fixed(54476:size(nexus4_trace_5V_400mA_fixed,1),3) = 0.025
 
 %% Aggregate & plot post-processed Nexus4 traces
 nexus4_max_S = 90000; % Change me, I am a hack
-nexus4_num_traces = 5; % Change me, I am a hack
+nexus4_num_traces = 6; % Change me, I am a hack
 nexus4_traces = NaN(nexus4_max_S, 3, nexus4_num_traces);
 
 % Skip raw nexus4 low current traces since they needed end-of-charge fix
@@ -41,14 +42,16 @@ nexus4_traces(:,:,1) = nexus4_trace_5V_300mA_fixed;
 % Skip raw nexus4 low current traces since they needed end-of-charge fix
 %nexus4_traces(:,:,2) = nexus4_trace_5V_400mA;
 nexus4_traces(:,:,2) = nexus4_trace_5V_400mA_fixed;
-nexus4_traces(:,:,3) = nexus4_trace_5V_600mA;
-nexus4_traces(:,:,4) = nexus4_trace_5V_700mA;
-nexus4_traces(:,:,5) = nexus4_trace_5V_1000mA;
+nexus4_traces(:,:,3) = nexus4_trace_5V_500mA;
+nexus4_traces(:,:,4) = nexus4_trace_5V_600mA;
+nexus4_traces(:,:,5) = nexus4_trace_5V_700mA;
+nexus4_traces(:,:,6) = nexus4_trace_5V_1000mA;
 % Skip 2100mA since not interesting
-%nexus4_traces(:,:,6) = nexus4_trace_5V_2100mA;
+%nexus4_traces(:,:,7) = nexus4_trace_5V_2100mA;
 
 nexus4_labels = {  'Nexus4, 5V, 300mA Limit -- Fixed'...
             'Nexus4, 5V, 400mA Limit -- Fixed'...
+            'Nexus4, 5V, 500mA Limit With USB Limit Removed'...
             'Nexus4, 5V, 600mA Limit With USB Limit Removed'...
             'Nexus4, 5V, 700mA Limit With USB Limit Removed'...
             'Nexus4, 5V, 1000mA Limit With USB Limit Removed'...
@@ -118,15 +121,54 @@ saveplot(gcf, [custom_post_processed_data_path 'charging_soc']);
 
 %% Aggregate & plot direct pre-processed battery traces
 rawbatt_max_S = 72000; % Change me, I am a hack
-rawbatt_num_traces = 1; % Change me, I am a hack
+rawbatt_num_traces = 4; % Change me, I am a hack
 rawbatt_traces = NaN(rawbatt_max_S, 3, rawbatt_num_traces);
 
 rawbatt_traces(:,:,1) = rawbatt_400mAh_trace_4200mV_500mA;
+rawbatt_traces(:,:,2) = rawbatt_400mAh_trace_4200mV_600mA;
+rawbatt_traces(:,:,3) = rawbatt_400mAh_trace_4200mV_700mA;
+rawbatt_traces(:,:,4) = rawbatt_400mAh_trace_4200mV_1000mA;
 
 rawbatt_labels = {  'Raw 400mAh Battery, 4.2V, 500mA Limit'...
+                    'Raw 400mAh Battery, 4.2V, 600mA Limit'...
+                    'Raw 400mAh Battery, 4.2V, 700mA Limit'...
+                    'Raw 400mAh Battery, 4.2V, 1000mA Limit'...
          };
 
 plot_charging_data(rawbatt_traces, hsv(rawbatt_num_traces), rawbatt_labels, 'Direct LiPo 400mAh Battery', rawbatt_post_processed_data_path);
+
+% For the 600mA run, we have both IV and SOC data. Let's make a special
+% plot for that.
+figure;
+[axes, line1, line2] = plotyy(rawbatt_400mAh_trace_4200mV_600mA(:,1)/3600, rawbatt_400mAh_trace_4200mV_600mA(:,2).*rawbatt_400mAh_trace_4200mV_600mA(:,3),...
+       rawbatt_400mAh_trace_4200mV_600mA_battsoc(:,1)/3600, rawbatt_400mAh_trace_4200mV_600mA_battsoc(:,2));
+ylabel(axes(1), 'Power (W)', 'FontName', 'Arial', 'FontSize', 16);
+ylabel(axes(2), 'Battery SOC (%)', 'FontName', 'Arial', 'FontSize', 16);
+xlabel(axes(2), 'Time (h)', 'FontName', 'Arial', 'FontSize', 16);
+title('Direct LiPo 400mAh Battery -- 4.2V, 600mA Limit', 'FontName', 'Arial', 'FontSize', 18);
+saveplot(gcf, [rawbatt_post_processed_data_path 'charging_power_and_soc']);
+
+%% Cleanup unnecessary data
+clear localeStruct;
+clear i;
+clear axes;
+clear line1;
+clear line2;
+clear tmp;
+
+% clear master_path;
+% 
+% clear pre_processed_data_path;
+% clear nexus4_pre_processed_data_path;
+% clear ipod_pre_processed_data_path;
+% clear custom_pre_processed_data_path;
+% clear rawbatt_pre_processed_data_path;
+% 
+% clear post_processed_data_path;
+% clear nexus4_post_processed_data_path;
+% clear ipod_post_processed_data_path;
+% clear custom_post_processed_data_path;
+% clear rawbatt_post_processed_data_path;
 
 %% Save post-processed workspace, which includes pre-processed data as well
 save([post_processed_data_path 'post-processed.mat'], '-v7.3');
--- preprocess_charging_data.m
+++ preprocess_charging_data.m
@@ -5,20 +5,26 @@
 master_path = '/Users/Mark/Dropbox/AvailabilityAware-share/data/charging/';
 
 raw_data_path = [master_path 'raw/']; 
+toshiba_raw_data_path = [raw_data_path 'toshiba/'];
 nexus4_raw_data_path = [raw_data_path 'nexus4/'];
 ipod_raw_data_path = [raw_data_path 'ipod/'];
 custom_raw_data_path = [raw_data_path 'custom/'];
 rawbatt_raw_data_path = [raw_data_path 'rawbatt/'];
 
 pre_processed_data_path = [master_path 'pre-processed/'];
+toshiba_pre_processed_data_path = [pre_processed_data_path 'toshiba/'];
 nexus4_pre_processed_data_path = [pre_processed_data_path 'nexus4/'];
 ipod_pre_processed_data_path = [pre_processed_data_path 'ipod/'];
 custom_pre_processed_data_path = [pre_processed_data_path 'custom/'];
 rawbatt_pre_processed_data_path = [pre_processed_data_path 'rawbatt/'];
 
+%% Parse raw Toshiba laptop traces
+%TODO
+
 %% Parse raw Nexus4 traces
 [nexus4_trace_5V_300mA] = convert_raw_keithley_csv([nexus4_raw_data_path 'nexus4-charging-nplc25-90000samples-5V-300mA-limit-notimestamp.csv'], 2, 60/25);
 [nexus4_trace_5V_400mA] = convert_raw_keithley_csv([nexus4_raw_data_path 'nexus4-charging-nplc25-90000samples-5V-400mA-limit-notimestamp.csv'], 2, 60/25);
+[nexus4_trace_5V_500mA] = convert_raw_keithley_csv([nexus4_raw_data_path 'nexus4-charging-nplc25-90000samples-5V-500mA-limit-modUSB-notimestamp.csv'], 2, 60/25);
 [nexus4_trace_5V_600mA] = convert_raw_keithley_csv([nexus4_raw_data_path 'nexus4-charging-nplc25-90000samples-5V-600mA-limit-modUSB-notimestamp.csv'], 2, 60/25);
 [nexus4_trace_5V_700mA] = convert_raw_keithley_csv([nexus4_raw_data_path 'nexus4-charging-nplc25-90000samples-5V-700mA-limit-modUSB-notimestamp.csv'], 2, 60/25);
 [nexus4_trace_5V_1000mA] = convert_raw_keithley_csv([nexus4_raw_data_path 'nexus4-charging-nplc25-90000samples-5V-1000mA-limit-modUSB-notimestamp.csv'], 2, 60/25);
@@ -27,6 +33,7 @@ rawbatt_pre_processed_data_path = [pre_processed_data_path 'rawbatt/'];
 %% Output pre-processed Nexus4 traces as CSV
 csvwrite([nexus4_pre_processed_data_path 'nexus4_trace_5V_300mA.csv'], nexus4_trace_5V_300mA);
 csvwrite([nexus4_pre_processed_data_path 'nexus4_trace_5V_400mA.csv'], nexus4_trace_5V_400mA);
+csvwrite([nexus4_pre_processed_data_path 'nexus4_trace_5V_500mA.csv'], nexus4_trace_5V_500mA);
 csvwrite([nexus4_pre_processed_data_path 'nexus4_trace_5V_600mA.csv'], nexus4_trace_5V_600mA);
 csvwrite([nexus4_pre_processed_data_path 'nexus4_trace_5V_700mA.csv'], nexus4_trace_5V_700mA);
 csvwrite([nexus4_pre_processed_data_path 'nexus4_trace_5V_1000mA.csv'], nexus4_trace_5V_1000mA);
@@ -52,8 +59,40 @@ csvwrite([custom_pre_processed_data_path 'custom_trace_5V_500mA_battsoc.csv'], c
 %% Parse Raw Battery traces
 [rawbatt_400mAh_trace_4200mV_500mA] = convert_raw_keithley_csv([rawbatt_raw_data_path 'rawbatt-charging-nplc6-72000samples-4.2V-500mA-limit-notimestamp.csv'], 2, 10);
 
+% The 600 mA run needs special handling because it is merged data from both
+% Keithley and the custom board modified to measure just SOC during
+% charging of rawbatt
+tmp = csvread([rawbatt_raw_data_path 'rawbatt-charging-4.2V-600mA-limit-VI-SOC-merged.csv'], 1, 0);
+rawbatt_400mAh_trace_4200mV_600mA = tmp(:,1:3); % First three columns of this special file are timestamp, voltage, current
+rawbatt_400mAh_trace_4200mV_600mA_battsoc = tmp(1:14429,4:5); % Last two columns of this special file are timestamp, battsoc. But sampling rate was different, so different # rows
+
+[rawbatt_400mAh_trace_4200mV_700mA] = convert_raw_keithley_csv([rawbatt_raw_data_path 'rawbatt-charging-nplc6-72000samples-4.2V-700mA-limit-notimestamp.csv'], 2, 10);
+[rawbatt_400mAh_trace_4200mV_1000mA] = convert_raw_keithley_csv([rawbatt_raw_data_path 'rawbatt-charging-nplc6-72000samples-4.2V-1000mA-limit-notimestamp.csv'], 2, 10);
+
 %% Output pre-processed Raw Battery traces as CSV
 csvwrite([rawbatt_pre_processed_data_path 'rawbatt_400mAh_trace_4.2V_500mA.csv'], rawbatt_400mAh_trace_4200mV_500mA);
+csvwrite([rawbatt_pre_processed_data_path 'rawbatt_400mAh_trace_4.2V_600mA.csv'], rawbatt_400mAh_trace_4200mV_600mA);
+csvwrite([rawbatt_pre_processed_data_path 'rawbatt_400mAh_trace_4.2V_600mA_battsoc.csv'], rawbatt_400mAh_trace_4200mV_600mA_battsoc);
+csvwrite([rawbatt_pre_processed_data_path 'rawbatt_400mAh_trace_4.2V_700mA.csv'], rawbatt_400mAh_trace_4200mV_700mA);
+csvwrite([rawbatt_pre_processed_data_path 'rawbatt_400mAh_trace_4.2V_1000mA.csv'], rawbatt_400mAh_trace_4200mV_1000mA);
+
+%% Cleanup unnecessary data
+clear localeStruct;
+clear i;
+
+% clear master_path;
+% 
+% clear raw_data_path; 
+% clear nexus4_raw_data_path;
+% clear ipod_raw_data_path;
+% clear custom_raw_data_path;
+% clear rawbatt_raw_data_path;
+% 
+% clear pre_processed_data_path;
+% clear nexus4_pre_processed_data_path;
+% clear ipod_pre_processed_data_path;
+% clear custom_pre_processed_data_path;
+% clear rawbatt_pre_processed_data_path;
 
 %% Save pre-processed workspace
 save([pre_processed_data_path 'pre-processed.mat'], '-v7.3');
